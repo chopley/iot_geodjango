692f694663466543759a8836176607ac14aa9f16
Charles Copley <charles@praekelt.org>
2021-04-20 19:41:30 -0400

    moved DB declaration to end of settings

--- iot_sensors/settings.py
+++ iot_sensors/settings.py
@@ -139,6 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
