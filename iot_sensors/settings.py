a84b8c8b1bf622d2c1c7b431a874b60f55a39cf7
Charles Copley <charles@praekelt.org>
2021-04-20 14:06:11 -0400

    trying to use heroku db

--- iot_sensors/settings.py
+++ iot_sensors/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
 # Application definition
 
@@ -81,14 +85,18 @@ WSGI_APPLICATION = 'iot_sensors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {'default':
-{'ENGINE': 'django.contrib.gis.db.backends.postgis',
-'NAME': 'gis',
-'USER': 'user001',
-'PASSWORD': '123456789',
-'HOST': 'localhost',
-'PORT': '5432'
-}}
+# DATABASES = {'default':
+# {'ENGINE': 'django.contrib.gis.db.backends.postgis',
+# 'NAME': 'gis',
+# 'USER': 'user001',
+# 'PASSWORD': '123456789',
+# 'HOST': 'localhost',
+# 'PORT': '5432'
+# }}
+
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
 # Password validation
