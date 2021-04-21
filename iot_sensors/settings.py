90bb60ed580b48f42d71e526971a94043afcf10d
Charles Copley <charles@praekelt.org>
2021-04-21 13:12:17 -0400

    trying to use different databases for heroku vs local

--- iot_sensors/settings.py
+++ iot_sensors/settings.py
@@ -85,18 +85,21 @@ WSGI_APPLICATION = 'iot_sensors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {'default':
-# {'ENGINE': 'django.contrib.gis.db.backends.postgis',
-# 'NAME': 'gis',
-# 'USER': 'user001',
-# 'PASSWORD': '123456789',
-# 'HOST': 'localhost',
-# 'PORT': '5432'
-# }}
+DATABASES_AVAILABLE = {
+    'default': {},
+    'tests':
+ {'ENGINE': 'django.contrib.gis.db.backends.postgis',
+ 'NAME': 'gis',
+ 'USER': 'user001',
+ 'PASSWORD': '123456789',
+ 'HOST': 'localhost',
+ 'PORT': '5432'
+ }
+}
 
 import dj_database_url
 django_heroku.settings(locals())
-DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
